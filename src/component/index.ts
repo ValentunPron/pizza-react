52e7c4919d1ac3353d5f7937acfe3d250fab376c
Valentyn <valentunpron@gmail.com>
2022-11-19 19:59:48 +0200

    API запрос

--- src/component/index.ts
+++ src/component/index.ts
@@ -2,5 +2,6 @@ import { Button } from "./Button";
 import { Header } from "./Header";
 import { Categories } from "./Categories";
 import { SortPoput } from "./SortPoput";
+import { PizzaItem } from "./PizzaItem";
 
-export { Button, Header, Categories, SortPoput };
+export { Button, Header, Categories, SortPoput, PizzaItem };
