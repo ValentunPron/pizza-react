bd6977702ee8744984ef1b6a9cedeed9876be805
Valentyn <valentunpron@gmail.com>
2022-12-06 22:00:22 +0200

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ import { Header } from './component';
 import { Home, Cart } from './pages';
 
 function App() {
-	const [test, setTest]: any = React.useState([]);
-
-	React.useEffect(() => {
-		axios('http://localhost:3000/db.json').then(({ data }) => setTest(data.pizzas));
-	}, [])
-
-	//console.log(test.sort((a: { price: any }, b: any) => a.price - b.price).reverse());
-	//console.log(test.sort((a: { rating: any }, b: any) => a.rating - b.rating).reverse());
-
-
 	return (
 		<div className="wrapper">
 			<Header />
